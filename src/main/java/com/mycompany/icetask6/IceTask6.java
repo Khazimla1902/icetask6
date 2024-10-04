b16930c98920783380c80f538146fd3aebe670f8
RC_Student_lab <RC_Student_lab@10.0.0.127>
2024-10-04 20:03:30 +0000

    first commit

---
+++ src/main/java/com/mycompany/icetask6/IceTask6.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.icetask6;
+import java.util.Scanner;
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class IceTask6 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String sentence;
+       
+    }
+}
